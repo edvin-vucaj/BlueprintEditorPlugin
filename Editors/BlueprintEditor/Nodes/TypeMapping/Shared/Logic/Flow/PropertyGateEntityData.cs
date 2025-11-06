1b770c89ca6cb46b5ac82930f282dbe911756979
edvin-vucaj <edvinvucaj@gmail.com>
2025-11-07 00:39:01 +0100

    Added "TransformIn" input port and "TransformOut" output port; removed "UIntIn" and "UIntOut" ports

--- Editors/BlueprintEditor/Nodes/TypeMapping/Shared/Logic/Flow/PropertyGateEntityData.cs
+++ Editors/BlueprintEditor/Nodes/TypeMapping/Shared/Logic/Flow/PropertyGateEntityData.cs
@@ -13,20 +13,20 @@ namespace BlueprintEditorPlugin.Editors.BlueprintEditor.Nodes.TypeMapping.Shared
 			AddInput("Open", ConnectionType.Event, Realm);
 			AddInput("Close", ConnectionType.Event, Realm);
 			AddInput("BoolIn", ConnectionType.Property, Realm);
-			AddInput("UintIn", ConnectionType.Property, Realm);
 			AddInput("IntIn", ConnectionType.Property, Realm);
-			AddInput("StringIn", ConnectionType.Property, Realm);
 			AddInput("FloatIn", ConnectionType.Property, Realm);
+			AddInput("StringIn", ConnectionType.Property, Realm);
 			AddInput("Vec3In", ConnectionType.Property, Realm);
-			AddInput("Default", ConnectionType.Property, Realm);
+            AddInput("TransformIn", ConnectionType.Property, Realm);
+            AddInput("Default", ConnectionType.Property, Realm);
 			AddInput("WritePropertyOnOpenGate", ConnectionType.Property, Realm);
 
 			AddOutput("BoolOut", ConnectionType.Property, Realm);
-			AddOutput("UintOut", ConnectionType.Property, Realm);
 			AddOutput("IntOut", ConnectionType.Property, Realm);
-			AddOutput("StringOut", ConnectionType.Property, Realm);
 			AddOutput("FloatOut", ConnectionType.Property, Realm);
+			AddOutput("StringOut", ConnectionType.Property, Realm);
 			AddOutput("Vec3Out", ConnectionType.Property, Realm);
-		}
+            AddOutput("TransformOut", ConnectionType.Property, Realm);
+        }
 	}
 }
