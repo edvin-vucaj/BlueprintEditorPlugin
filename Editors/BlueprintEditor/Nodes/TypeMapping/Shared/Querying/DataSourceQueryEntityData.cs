c23d20740b244500fb2481dc101dea5519c81dbe
edvin-vucaj <edvinvucaj@gmail.com>
2025-11-07 20:38:11 +0100

    Added necessary input/output ports for entities: DataSourceQuery, LocalizedStringIdPicker on object creation

---
+++ Editors/BlueprintEditor/Nodes/TypeMapping/Shared/Querying/DataSourceQueryEntityData.cs
@@ -0,0 +1,18 @@
+﻿using BlueprintEditorPlugin.Editors.BlueprintEditor.Connections;
+
+namespace BlueprintEditorPlugin.Editors.BlueprintEditor.Nodes.TypeMapping.Shared.Querying
+{
+    public class DataSourceQueryEntityData : EntityNode
+    {
+        public override string ObjectType => "DataSourceQueryEntityData";
+
+        public override string ToolTip => "This node queries different kinds of data from asset files and outputs them as dynamic data types";
+
+        public override void OnCreation()
+        {
+            base.OnCreation();
+
+            AddInput("InData", ConnectionType.Property, Realm);
+        }
+    }
+}
---
+++ Editors/BlueprintEditor/Nodes/TypeMapping/Shared/Querying/LocalizedStringIdPickerEntityData.cs
@@ -0,0 +1,18 @@
+﻿using BlueprintEditorPlugin.Editors.BlueprintEditor.Connections;
+
+namespace BlueprintEditorPlugin.Editors.BlueprintEditor.Nodes.TypeMapping.Shared.Querying
+{
+    internal class LocalizedStringIdPickerEntityData : EntityNode
+    {
+        public override string ObjectType => "LocalizedStringIdPickerEntityData";
+
+        public override string ToolTip => "This node outputs a DynamicStringId based on the Sid given. The format of Sid is \"ID_*\"";
+
+        public override void OnCreation()
+        {
+            base.OnCreation();
+
+            AddOutput("StringId", ConnectionType.Property, Realm);
+        }
+    }
+}
