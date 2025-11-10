44e1bf3736b5f62257a2d336123634cd53458685
edvin-vucaj <edvinvucaj@gmail.com>
2025-11-10 15:34:53 +0100

    Added SelectProperty entity

---
+++ Editors/BlueprintEditor/Nodes/TypeMapping/Shared/Selection/SelectPropertyEntityData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BlueprintEditorPlugin.Editors.BlueprintEditor.Connections;
+
+namespace BlueprintEditorPlugin.Editors.BlueprintEditor.Nodes.TypeMapping.Shared.Selection
+{
+    public class SelectPropertyEntityData : EntityNode
+    {
+        public override void OnCreation()
+        {
+            base.OnCreation();
+
+            // Source field Ids
+            AddInput("Out", ConnectionType.Property, Realm);
+            AddInput("SelectedIndex", ConnectionType.Property, Realm);
+
+            // Source events
+            AddOutput("OnSelected", ConnectionType.Event, Realm);
+        }
+    }
+}
